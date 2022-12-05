776beaf133b59d67ae776d58cae5037b67a4a848
ashwithkumar513 <119408675+ashwithkumar513@users.noreply.github.com>
2022-12-05 19:39:02 +0530

    Update N_bit_adder.v

--- verilog/rtl/N_bit_adder.v
+++ verilog/rtl/N_bit_adder.v
@@ -1,22 +1,22 @@
 `timescale 1ns / 1ps
 
 module N_bit_adder(input1,input2,answer);
-parameter N =20;
-input [N-1:0] input1,input2 ;
-   output [N-1:0] answer ;
+
+   input [19:0] input1,input2 ;
+   output [19:0] answer ;
   
    wire  carry_out;
-  wire [N-1:0] carry;
+   wire [19:0] carry;
    genvar i;
    generate 
-   for(i=0;i<N;i=i+1)
+      for(i=0;i<20;i=i+1)
      begin: generate_N_bit_Adder
    if(i==0) 
   half_adder f(input1[0],input2[0],answer[0],carry[0]);
    else
   full_adder f(input1[i],input2[i],carry[i-1],answer[i],carry[i]);
      end
-  assign carry_out = carry[N-1];
+      assign carry_out = carry[19];
    endgenerate
 endmodule 
 
