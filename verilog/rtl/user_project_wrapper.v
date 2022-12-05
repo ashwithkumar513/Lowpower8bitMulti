8b1942f067272bde8caea9fe40d9f1fef15179e3
ashwithkumar513 <119408675+ashwithkumar513@users.noreply.github.com>
2022-12-05 16:07:58 +0530

    Update user_project_wrapper.v

--- verilog/rtl/user_project_wrapper.v
+++ verilog/rtl/user_project_wrapper.v
@@ -82,7 +82,7 @@ module user_project_wrapper #(
 /* User project is instantiated  here   */
 /*--------------------------------------*/
 
-fibonacci mprj (
+main mprj (
 //`ifdef USE_POWER_PINS
 //	.vdd(vdd),	// User area 1 1.8V power
 //	.vss(vss),	// User area 1 digital ground
